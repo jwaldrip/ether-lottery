67b0a18b22fc3bbae61f96b991bf04571d9bef94
jwaldrip <jason@waldrip.net>
2017-06-14 21:24:03 -0600

    dapp init EtherLottery

---
+++ src/EtherLottery.t.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.11;
+
+import "ds-test/test.sol";
+
+import "./EtherLottery.sol";
+
+contract EtherLotteryTest is DSTest {
+    EtherLottery lottery;
+
+    function setUp() {
+        lottery = new EtherLottery();
+    }
+
+    function testFail_basic_sanity() {
+        assert(false);
+    }
+
+    function test_basic_sanity() {
+        assert(true);
+    }
+}
